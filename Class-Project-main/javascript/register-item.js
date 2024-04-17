0f692d010065c340cbd4a410d0c6b3fed1fe4de3
owlyhousenskyi <dritachitta@gmail.com>
2024-04-17 16:01:37 -0400

    Initialize JavaScript project with LFS

---
+++ Class-Project-main/javascript/register-item.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4754be3acedcdb2c9569fba3cc8d31a60e1d4e80697c5dfc92826210f4e361b5
+size 9383
---
+++ README.txt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:29a9ea92b26b17c3519fa141c679e7e871eb9ba3d106c39be8063bd8db023ab2
+size 97
